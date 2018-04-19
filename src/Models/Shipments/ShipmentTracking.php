861911a0affbef72c06e7ed69f19021cf3a88c18
Morten Harders <mh@coolrunner.dk>
2018-04-19 09:07:33 +0200

    Added tracking models

---
+++ src/Models/Shipments/ShipmentTracking.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @package   api-v3-sdk
+ * @author    Morten Harders
+ * @copyright 2018
+ */
+
+namespace CoolRunnerSDK\Models\Shipments;
+
+/**
+ * Class ShipmentTracking
+ *
+ * @property string                  $package_number
+ * @property string                  $carrier
+ * @property ShipmentTrackingEntry[] $tracking
+ *
+ * @package CoolRunnerSDK\Models\Shipments
+ */
+class ShipmentTracking
+    implements \ArrayAccess, \Iterator, \Countable {
+    protected $package_number, $carrier;
+
+    /** @var ShipmentTrackingEntry[] */
+    protected $tracking = array();
+
+    public function __construct($data) {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                if ($key !== 'tracking') {
+                    $this->{$key} = $value;
+                } else {
+                    $this->{$key} = ShipmentTrackingEntry::getList($value);
+                }
+            }
+        }
+    }
+
+    public function __get($name) {
+        return isset($this->{$name}) ? $this->{$name} : null;
+    }
+
+    public function __set($name, $value) {
+        if (property_exists($this, $name)) {
+            $this->{$name} = !isset($this->{$name}) ? $value : $this->{$name};
+        }
+    }
+
+    // region ArrayAccess
+
+    public function offsetExists($offset) {
+        return isset($this->tracking[$offset]);
+    }
+
+    /**
+     * @return bool|ShipmentTrackingEntry|mixed
+     */
+    public function offsetGet($offset) {
+        return $this->offsetExists($offset) ? $this->tracking[$offset] : false;
+    }
+
+    public function offsetSet($offset, $value) {
+        $this->tracking[$offset] = $value;
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->tracking[$offset]);
+    }
+
+    // endregion
+
+    //region Countable
+
+    /**
+     * @inheritdoc
+     */
+    public function count() {
+        return count($this->tracking);
+    }
+
+    // endregion
+
+    // region Iterator
+
+    /**
+     * @inheritdoc
+     */
+    public function rewind() {
+        return reset($this->tracking);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function current() {
+        return current($this->tracking);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function next() {
+        return next($this->tracking);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function key() {
+        return key($this->tracking);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function valid() {
+        return $this->key() !== null && $this->key() !== false;
+    }
+
+    // endregion
+}
---
+++ src/Models/Shipments/ShipmentTrackingEntry.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package   api-v3-sdk
+ * @author    Morten Harders
+ * @copyright 2018
+ */
+
+namespace CoolRunnerSDK\Models\Shipments;
+
+/**
+ * Class ShipmentTrackingEntry
+ *
+ * @property string $timestamp
+ * @property string $title
+ * @property string $event
+ * @property string $location
+ *
+ * @package CoolRunnerSDK\Models\Shipments
+ */
+class ShipmentTrackingEntry {
+    protected $timestamp, $title, $event, $location;
+
+
+    public function __construct($data) {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    /**
+     * This method compiles a list of ShipmentTrackingEntries
+     *
+     * @param $data
+     *
+     * @return array
+     */
+    public static function getList($data) {
+        $ret = array();
+        foreach ($data as $event_info) {
+            $ret[] = new self($event_info);
+        }
+        return $ret;
+    }
+
+    public function __get($name) {
+        return isset($this->{$name}) ? $this->{$name} : null;
+    }
+
+    public function __set($name, $value) {
+        if (property_exists($this, $name)) {
+            $this->{$name} = !isset($this->{$name}) ? $value : $this->{$name};
+        }
+    }
+
+    /**
+     * Get the timestamp as a DateTime object
+     *
+     * @return bool|\DateTime
+     */
+    public function getDateTime() {
+        return \DateTime::createFromFormat('Y-m-d H:i', $this->timestamp);
+    }
+}
